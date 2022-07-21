4e485674c1a000a9bc5b5be440dac9c08d185e05
Nikita Kudenikov <KudenikovNS@yandex.ru>
2022-07-21 18:31:35 +0400

    cors1

--- middlewares/cros.js
+++ middlewares/cros.js
@@ -1,10 +1,7 @@
 const allowedCors = [
   'kudenikovns.studens.nomorepartiesxyz.ru',
-  'api.kudenikovns.studens.nomorepartiesxyz.ru',
   'http://kudenikovns.studens.nomorepartiesxyz.ru',
-  'http://api.kudenikovns.studens.nomorepartiesxyz.ru',
   'https://kudenikovns.studens.nomorepartiesxyz.ru',
-  'https://api.kudenikovns.studens.nomorepartiesxyz.ru',
   'localhost:3000',
 ];
 
