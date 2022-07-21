10b0b616c995b54948435532af8a8dfd8be2b8d0
Nikita Kudenikov <KudenikovNS@yandex.ru>
2022-07-21 11:15:36 +0400

    Add cros +

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const userRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { validationForLink } = require('./utils/validationForLink');
 const NotFoundError = require('./errors/NotFoundError');
+const cors = require('./middlewares/cros');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
@@ -18,6 +19,7 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+app.use(cors);
 app.use(requestLogger);
 app.use(cookieParser());
 app.use(express.json());
