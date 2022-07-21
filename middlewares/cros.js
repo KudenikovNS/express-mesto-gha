6e5e82328b58db277f60b9db2bb0d80fbd570311
Nikita Kudenikov <KudenikovNS@yandex.ru>
2022-07-21 18:22:13 +0400

    cors

--- middlewares/cros.js
+++ middlewares/cros.js
@@ -1,27 +1,28 @@
 const allowedCors = [
-  'https://kudenikovns.studens.nomorepartiesxyz.ru',
+  'kudenikovns.studens.nomorepartiesxyz.ru',
+  'api.kudenikovns.studens.nomorepartiesxyz.ru',
   'http://kudenikovns.studens.nomorepartiesxyz.ru',
+  'http://api.kudenikovns.studens.nomorepartiesxyz.ru',
+  'https://kudenikovns.studens.nomorepartiesxyz.ru',
+  'https://api.kudenikovns.studens.nomorepartiesxyz.ru',
   'localhost:3000',
-  'http://localhost:3000',
 ];
 
-// eslint-disable-next-line consistent-return
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.status(200).end();
+    if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      res.end();
+    }
   }
-
   next();
 };
