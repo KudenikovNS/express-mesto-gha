e0a25f4e1ff5c40ac0ed135a9c22afa58cbcb0a4
Nikita Kudenikov <KudenikovNS@yandex.ru>
2022-07-22 14:19:24 +0400

    errorHandler1

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,7 +1,5 @@
-module.exports = (err, req, res, next) => {
+module.exports = ((err, req, res, next) => {
   const { statusCode = 500, message } = err;
-  res.send(statusCode
-    ? { message: 'На сервере произошла ошибка' }
-    : { message });
+  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
   next();
-};
+});
