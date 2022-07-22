ade1bf19e0f00fa30043a8fff392028c432358c5
Nikita Kudenikov <KudenikovNS@yandex.ru>
2022-07-22 18:50:43 +0400

    12

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -1,10 +1,5 @@
-const { DEFAUTL_CODE } = require('../utils/constants');
-
 module.exports = (err, req, res, next) => {
-  const { statusCode = DEFAUTL_CODE, message } = err;
-
-  res.status(statusCode).send(statusCode === DEFAUTL_CODE
-    ? { message: 'На сервере произошла ошибка' }
-    : { message });
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
   next();
 };
